3b67c1d39a0d9f316b55e5757d0ccd2f9022909a
Raedin24 <lexhue24@gmail.com>
2023-09-04 15:57:47 +0000

    04/09/2023

---
+++ 237.delete-node-in-a-linked-list.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=237 lang=python3
+#
+# [237] Delete Node in a Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node
+        
+# @lc code=end
+
