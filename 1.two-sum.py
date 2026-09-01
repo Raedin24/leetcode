7d3812bd89032ff2f1bcc627ce07a8cbe58a8a7b
Raedin <lexhue24@gmail.com>
2026-09-01 16:17:07 +0000

    Updated with one-pass hashmap solution

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -9,19 +9,34 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        i = 0
-        while i <= len(nums) - 1:
-            j = i + 1
-            while j < len(nums):
-                if nums[i] + nums[j] == target:
-                    return i, j
-                j += 1
-            i += 1
+        # Brute force solution
+        # Time complexity: O(n^2)
+        # Space complexity: O(1)
+        # i = 0
+        # while i <= len(nums) - 1:
+        #     j = i + 1
+        #     while j < len(nums):
+        #         if nums[i] + nums[j] == target:
+        #             return i, j
+        #         j += 1
+        #     i += 1
+
+        # One-pass hashmap solution
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+        hashmap = {}
+        for position, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap:
+                result = [position, hashmap[complement]]
+                return result
+            hashmap[num] = position
+        return []
 
         
 # @lc code=end
 
 # Test cases
-# sol = Solution()
-# nums = [3,2,4]
-# print(sol.twoSum(nums, 6))
+sol = Solution()
+nums = [3,2,4]
+print(sol.twoSum(nums, 6))
